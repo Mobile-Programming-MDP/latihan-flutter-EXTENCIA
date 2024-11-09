3acfe03f4948a097cf0cea82745d5a5d535d5e15
EXTENCIA <dwibayulaksmnrmd22@gmail.com>
2024-11-09 12:32:39 +0700

    Update Search Screen

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/detail_screen.dart';
 import 'package:wisata_candi/profil_screen.dart';
+import 'package:wisata_candi/search_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,7 +14,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ProfileScreen()
+      home: SearchScreen()
     );
   }
 }
